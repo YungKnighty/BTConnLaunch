372930ba792af881658fc24fb414221300720ca1
YungKnighty <danielcamcameron@gmail.com>
2024-02-07 16:38:59 -0500

    basic functionality
    - wrap in main function
    - forego constants for variables
    - debugging statements + logic
    - simplify process find logic (eventually file picker)

--- BTConnLaunch.ps1
+++ BTConnLaunch.ps1
@@ -1,33 +1,84 @@
-# Set powershell directory to script's root
-Set-Location $PSScriptRoot
+Function Main {
+	# Set powershell directory to script's root
+	Set-Location $PSScriptRoot
 
-# MessageBox functionality
-Add-Type -AssemblyName System.Windows.Forms
+	# MessageBox functionality
+	Add-Type -AssemblyName System.Windows.Forms
 
+	# TODO implement first time setup + later config
+
+	# Init variables
+	$AppName = "check"
+	$AppFileExtension = "exe"
+	$ExcludedName = "DS4Updater.exe"
+	$BluetoothDeviceName = "Wireless Controller"
+	$Debugging = $false
+
+	# TODO implement file picker HACK JOB >:(
+	$AppPath = Get-AppPath
+
+	# Get BT Connectivity Data
+	$BtStatus = Get-PnpDevice -class Bluetooth -FriendlyName $BluetoothDeviceName | 
+	Get-PnpDeviceProperty -KeyName '{83DA6326-97A6-4088-9453-A1923F573B29} 15' |
+	Select-Object -ExpandProperty Data
+
+	# TODO make prettier dividers and columns for logging, use delims
+	# $BluetoothDeviceName is connected
+	If ($BtStatus) {
+		if ($Debugging) { Set-Log "Info" "Connection:        $BluetoothDeviceName - FOUND" }
+		
+		# Start process if not already running
+		if (-not (Get-Process -ErrorAction SilentlyContinue |
+		Where-Object { $_.ProcessName -ne $ExcludedName -and $_.Path -eq $AppPath })) {
+			Start-Process -FilePath $AppPath
+
+			if ($Debugging) { Set-Log "Info" "Process:           $AppName.$AppFileExtension - START" }
+		}
+		
+	# $BluetoothDeviceName not connected
+	} else {
+		if ($Debugging) { Set-Log "Info" "Connection:        $BluetoothDeviceName - NOT FOUND" }
+
+		if (Get-Process -Name $AppName -ErrorAction SilentlyContinue |
+		Where-Object { $_.ProcessName -ne $ExcludedName -and $_.Path -eq $AppPath }) {
+			
+			# Force stop process
+			Get-Process -Name $AppName -ErrorAction SilentlyContinue |
+			Where-Object { $_.ProcessName -like $AppName -and $_.ProcessName -ne $ExcludedName } |
+			Stop-Process -Force
+
+			if ($Debugging) { Set-Log "Info" "Process:           $AppName.$AppFileExtension - STOP" }
+		}
+	}
+
+	if ($Debugging) {
+		pause
+	} else {
+		exit
+	}
+}
+
+# TODO finish comments
 <#
-    .SYNOPSIS
-    Log an event to current directory.
+.SYNOPSIS
+Log an event to current directory.
 
-    .DESCRIPTION
-    When called, Log-Message writes the current time and given parameter, $Message, to a BTConnLog.txt file in the script's current 
-	directory (it creates this file if it does not currently exist).
+.DESCRIPTION
+When called, Log-Message writes the current time and given parameter, $Message, to a BTConnLog.txt file in the script's current 
+directory (it creates this file if it does not currently exist).
 
-    .PARAMETER Ip
-    IP address of the OME server
+.PARAMETER x
 
-    .PARAMETER Credentials
-    Credentials for the OME server
+.PARAMETER x
 
-    .INPUTS
-    None. You cannot pipe objects to Invoke-Authenticate.
+.INPUTS
+x
 
-    .OUTPUTS
-    hashtable. The Invoke-Authenticate function returns a hashtable with the headers resulting from authentication 
-               against the OME server
+.OUTPUTS
+x
 
-  #>
-Function Set-Log
-{
+#>
+Function Set-Log {
 	Param (
 		[Parameter(Mandatory)]
 		[ValidateSet("Success","Fail","Info")]
@@ -35,56 +86,39 @@ Function Set-Log
 		[Parameter(Mandatory)]
 		[String]$Message
 	)
-	if (-not [System.IO.File]::Exists("$PSScriptRoot\BTConnDebug.log")) {
-		
-	}
 	
+	# Space formatting
 	$StatusFormat = if ($Status -ne "Success") { "$Status   " } else { $Status }
-    Add-Content -Path "$PSScriptRoot\BTConnLog.log" -Value "$(Get-Date -Format `"dd/MM/yyyy | HH:mm:ss`") | $StatusFormat | $Message"
+	# Write to log
+	Add-Content -Path "$PSScriptRoot\BTConnLog.log" -Value "$(Get-Date -Format `"dd/MM/yyyy | HH:mm:ss`") | $StatusFormat | $Message"
 }
 
+# TODO make comments
 Function Get-AppPath {
-	($ApplicationPath = Get-ChildItem -Filter ($ApplicationProcessName + ".exe") -File | 
+	# Find first application's file path in current working dir
+	# Exclude indicated variable from search
+	($AppPath = Get-ChildItem -Filter ("$AppName.$AppFileExtension") -File | 
 						Where-Object { $_.Name -ne $ExcludedName } |
 						Select-Object -ExpandProperty FullName -First 1)
-	if ($null -eq $ApplicationPath) {
-        $msgBoxInput = [System.Windows.Forms.MessageBox]::Show("Specified application (" + $ApplicationProcessName + ".exe) not found. Try searching again?",
-															"Bluetooth Launch Error",
+	
+	if ($null -eq $AppPath) {
+		if ($Debugging) { Set-Log "Fail" "$AppName.$AppFileExtension not found in current working directory." }
+		
+		# Prompt for application search or exit
+		$msgBoxInput = [System.Windows.Forms.MessageBox]::Show("Specified application (" + $AppName + "." + $AppFileExtension + ") not found. Try searching again?",
+															"Application Not Found",
 															[System.Windows.Forms.MessageBoxButtons]::RetryCancel,
 															"Error")
+		# Reattempt search
 		if ($msgBoxInput -eq "Retry") {
 			Get-AppPath
-		} elseif ($msgBoxInput -eq "Cancel") {
-			exit
+		} 
+		elseif ($msgBoxInput -eq "Cancel") {
+			if ($Debugging) { pause } else { exit }
 		}
+	} else {
+		if ($Debugging) { Set-Log "Success" "ApplicationPath:   $AppPath" }
 	}
 }
 
-# Init variables
-Set-Variable -Name ApplicationProcessName -Option Constant -value ([string]"DS4*") -ErrorAction Ignore
-Set-Variable -Name ExcludedName -Option Constant -value ([string]"DS4Updater.exe") -ErrorAction Ignore
-Set-Variable -Name BluetoothDeviceName -Option Constant -value ([string]"Wireless Controller") -ErrorAction Ignore
-Set-Log "Info" "Hi"
-# Get BT Connectivity Data
-$BtStatus = Get-PnpDevice -class Bluetooth -FriendlyName $BluetoothDeviceName | 
-  Get-PnpDeviceProperty -KeyName '{83DA6326-97A6-4088-9453-A1923F573B29} 15' |
-  Select-Object -ExpandProperty Data
-
-# $BluetoothDeviceName Connected
-If ($BtStatus) {
-	$ApplicationPath = Check-ApplicationExist
-	
-	# Start process if not already running
-	if (-not (Get-Process -Name $ApplicationProcessName -ErrorAction SilentlyContinue |
-				Where-Object { $_.ProcessName -like $ApplicationProcessName -and $_.ProcessName -ne $ExcludedName })) {
-		Start-Process -FilePath $ApplicationPath
-	}
-	
-# $BluetoothDeviceName Disconnected
-} else {
-	# Force stop process
-	Get-Process -Name $ApplicationProcessName -ErrorAction SilentlyContinue |
-		Where-Object { $_.ProcessName -like $ApplicationProcessName -and $_.ProcessName -ne $ExcludedName } |
-		Stop-Process -Force
-}
-exit
+Main
--- test.ps1
+++
@@ -1,4 +0,0 @@
-$wshell = New-Object -ComObject Wscript.Shell
-
-$wshell.Popup("Operation Completed",0,"Done",0)
-exit
